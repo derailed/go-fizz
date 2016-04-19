c3eeac720d4e9bbed86fb4a2bf6362b16abb5d03
derailed <fernand.galiana@gmail.com>
2016-04-19 16:03:31 -0600

    cleaning up

--- main.go
+++ main.go
@@ -3,17 +3,17 @@ package main
 
 import (
 	"flag"
-    "fmt"
-    
-    "github.com/derailed/go-fizz/buzz"
+	"fmt"
+
+	"github.com/derailed/go-fizz/buzz"
 )
 
 func main() {
-    var total = flag.Int("t", 20, "Bizzbuzz iterations ")
-    flag.Parse()
-    
-    res := buzz.NewGame(*total).Play()
-    for i, r := range res {
-        fmt.Printf("%02d\t%s\n", i, r)
-    }
+	var total = flag.Int("t", 20, "Bizzbuzz iterations ")
+	flag.Parse()
+
+	res := buzz.NewGame(*total).Play()
+	for i, r := range res {
+		fmt.Printf("%02d\t%s\n", i, r)
+	}
 }
